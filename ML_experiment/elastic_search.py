a19004ced97cdbcec2846049d40888e314a58c48
vikrant <vikrant@dell.local>
2020-02-17 12:58:28 +0530

    add comment and remove unwanted data

--- ML_experiment/elastic_search.py
+++ ML_experiment/elastic_search.py
@@ -3,25 +3,15 @@ from elasticsearch import Elasticsearch
 import pandas as pd
 es = Elasticsearch()
 
-df=pd.read_csv("/home/vikrant/Downloads/vikrant/Ardent/Data_CSV/sherwood.csv")
-
-# print(df.head())df.shape[0]df.shape[0]
-
-#insert below data into elasticsearchdf.shape[0]
-# doc = {
-#     'author': 'kimchy',
-#     'text': 'Elasticsearch: cool. bonsai cool.',
-#     'timestamp': datetime.now(),
-# }
-# res = es.index(index="test-index", id=1, body=doc)
-# print(res['result'])
+df=pd.read_csv("/home/vikrant/Downloads/test csv/test3.csv")
 
 #delete index
 # es.indices.delete(index='test-index', ignore=[400, 404])
 
-
+#delete the data from index not delete index
+# es.delete_by_query(index="testx", body={"query":{"range":{"Count":{"gte":0}}}})
 
 #push csv into elasticsearch
-df['Id'] = range(0, df.shape[0])
+# df['Id'] = range(0, df.shape[0])    #create id which is not present in crawler generated csv
 for i in range(df.shape[0]):
-    es.index(index="sherwood", body=df.iloc[i].to_json())
+    es.index(index="testx", body=df.iloc[i].to_json())
